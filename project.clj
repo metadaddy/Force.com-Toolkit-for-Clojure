d337779929fd1352f8e1d6be5219813129ee34bb
Pat Patterson <ppatterson@salesforce.com>
2013-03-25 12:47:26 -0700

    Get correct clj-oauth2 version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 								 [clj-http "0.2.6"] ; for clj-oauth2
 								 [uri "1.1.0"] ; for clj-oauth2
                  [commons-codec/commons-codec "1.6"] ; for clj-oauth2
-								 [clj-oauth2 "0.3.0"]
+								 [antler/clj-oauth2 "0.3.0"]
                  [enlive "1.0.0"]]
   :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :main clojure-force.core)
